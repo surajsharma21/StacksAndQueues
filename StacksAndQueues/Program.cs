e3b56b4231330cbd11592e8f4c9349f531cd0638
Suraj <surajshrm000@gmail.com>
2020-10-28 23:20:04 +0530

    [Suraj] Add: Ability to create a stack and push any item to it

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.push(12);
+            stack.push(13);
+            stack.push(14);
+            stack.Display();
+        }
+    }
+}
---
+++ StacksAndQueues/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    public class Stack
+    {
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        public void push(int item)
+        {
+            Node node = new Node(item);
+            if (top == null)
+            {
+                top = node;
+            }
+            else
+            {
+                node.next = top;
+                top = node;
+            }
+        }
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
