f0935c1b22c42baafe0b029bf188f4fdf8d61d87
Suraj <surajshrm000@gmail.com>
2020-11-01 18:26:22 +0530

    [Suraj] Add: Enqueue Operation in Queue

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -6,6 +6,7 @@ namespace StacksAndQueues
     {
         static void Main(string[] args)
         {
+            //Stack Operations
             Stack stack = new Stack();
             stack.Push(70);
             stack.Push(30);
@@ -15,6 +16,12 @@ namespace StacksAndQueues
             stack.Pop();
             stack.IsEmpty();
             stack.Display();
+           //Queue Operations
+            Queue linkedListQueue = new Queue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
         }
     }
 }
